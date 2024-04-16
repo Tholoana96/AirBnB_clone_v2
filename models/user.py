35fdd6c31dd1a6560fbd21c93c13ba796a6d9467
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-04-16 15:02:20 +0200

    task 7

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defines the User class."""
-from sqlalchemy import Column, String
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
