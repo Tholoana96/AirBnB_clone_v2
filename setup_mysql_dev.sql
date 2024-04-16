4276ceca75e182d351402a5c9127864ae8f72fd3
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-04-16 12:14:22 +0200

    task 3 fixing error

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,18 +1,12 @@
--- creates a MySQL server with:
--- database hbnb_dev_db.
--- user hbnb_dev with password hbnb_dev_pwd in localhost.
--- grants all privileges for hbnb_dev on hbnb_dev_db.
--- grants SELECT privilege for hbnb_dev on performance_schema.
+-- create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+-- create the user if it doesn't exist, and set the password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- revoke all privileges on performance_schema from the hbnb_dev user (if any)
+REVOKE ALL PRIVILEGES ON performance_schema.* FROM 'hbnb_dev'@'localhost';
+-- grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- flush privileges to apply the changes
 FLUSH PRIVILEGES;
