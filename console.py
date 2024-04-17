ba5a0a2f789baaaec6ecdcdb66fad5912377055c
Nomcebo1992 <snembambo@gmail.com>
2024-04-17 16:41:44 -0700

    console.py

--- console.py
+++ console.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
-from datetime import datetime
+import sys
 import re
 import os
-import sys
+from datetime import datetime
 import uuid
-
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
@@ -107,6 +106,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """ Handles EOF to exit program """
+        print()
         exit(0)
 
     def help_EOF(self):
@@ -115,7 +115,7 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """ Overrides the emptyline method of CMD """
-        return False
+        pass
 
     def do_create(self, args):
         """ Create an object of any class"""
@@ -297,61 +297,73 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
 
+        # isolate id from args
         args = args[2].partition(" ")
         if args[0]:
             c_id = args[0]
-        else:
+        else:  # id not present
             print("** instance id missing **")
             return
 
+        # generate key from class and id
         key = c_name + "." + c_id
 
+        # determine if key is present
         if key not in storage.all():
             print("** no instance found **")
             return
 
+        # first determine if kwargs or args
         if '{' in args[2] and '}' in args[2] and type(eval(args[2])) is dict:
             kwargs = eval(args[2])
-            args = []
+            args = []  # reformat kwargs into list, ex: [<name>, <value>, ...]
             for k, v in kwargs.items():
                 args.append(k)
                 args.append(v)
-        else:
+        else:  # isolate args
             args = args[2]
-            if args and args[0] == '\"':
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
 
             args = args.partition(' ')
 
+            # if att_name was not quoted arg
             if not att_name and args[0] != ' ':
                 att_name = args[0]
+            # check for quoted val arg
             if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
+            # if att_val was not quoted arg
             if not att_val and args[2]:
                 att_val = args[2].partition(' ')[0]
 
             args = [att_name, att_val]
 
-            new_dict = storage.all()[key]
+        # retrieve dictionary of current objects
+        new_dict = storage.all()[key]
 
+        # iterate through attr names and values
         for i, att_name in enumerate(args):
+            # block only runs on even iterations
             if (i % 2 == 0):
-                att_val = args[i + 1]
-                if not att_name:
+                att_val = args[i + 1]  # following item is value
+                if not att_name:  # check for att_name
                     print("** attribute name missing **")
                     return
-                if not att_val:
+                if not att_val:  # check for att_value
                     print("** value missing **")
                     return
+                # type cast as necessary
                 if att_name in HBNBCommand.types:
                     att_val = HBNBCommand.types[att_name](att_val)
 
+                # update dictionary with name, value pair
                 new_dict.__dict__.update({att_name: att_val})
 
-        new_dict.save()
+        new_dict.save()  # save updates to file
 
     def help_update(self):
         """ Help information for the update class """
